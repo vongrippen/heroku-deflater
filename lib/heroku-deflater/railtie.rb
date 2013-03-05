54e1a833a9e4aacd6e2634ba7d9983e301de5a66
Mike Cochran <mcochran@linux.com>
2013-03-05 12:07:40 -0600

    Fix for no static assets on initial deploy

--- lib/heroku-deflater/railtie.rb
+++ lib/heroku-deflater/railtie.rb
@@ -4,8 +4,8 @@ require 'heroku-deflater/skip_binary'
 module HerokuDeflater
   class Railtie < Rails::Railtie
     initializer "heroku_deflater.configure_rails_initialization" do |app|
-      app.middleware.insert_before ActionDispatch::Static, Rack::Deflater
-      app.middleware.insert_before ActionDispatch::Static, HerokuDeflater::SkipBinary
+      app.middleware.insert_before Rack::Cache, Rack::Deflater
+      app.middleware.insert_before Rack::Cache, HerokuDeflater::SkipBinary
     end
 
     # Set default Cache-Control headers to one week.
